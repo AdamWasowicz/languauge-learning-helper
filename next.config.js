e6683d3840b6b54a98f2d330f08716eb557b492f
Adam <adam.p.wasowicz@gmail.com>
2024-02-20 17:37:15 +0100

    docker

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const environment = process.env.NODE_ENV;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     sassOptions: { includePaths: [path.join(__dirname, 'styles')], },
+    output: 'standalone'
 };
 
 
